d2fc4af3a950bfc569875817911979de24c3004b
lucamarius <luca.marius.daniel@gmail.com>
2020-12-06 21:10:06 +0200

    Provider Class cleaned

--- src/main/java/com/shop/model/entity/Provider.java
+++ src/main/java/com/shop/model/entity/Provider.java
@@ -1,8 +1,6 @@
 package com.shop.model.entity;
 
-<<<<<<< HEAD:src/main/java/com/shop/model/entity/Provider.java
+
 public class Provider {
-=======
-public class Delivery {
->>>>>>> 98d466e06726f56e3242fba042bd6b75fcb9040d:src/main/java/com/shop/model/entity/Delivery.java
+
 }
